aa3edbd7c9f8f660fa0c95740ca61b61acdd6730
ChuckGao <chuckgao0223@gmail.com>
2022-10-08 01:59:43 +0800

    🚀 [feat] bot first done

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,76 @@
 import Request from '../utils/request'
 
+interface IPostTemplate {
+  title: string
+  text: string
+  aText: string
+  aHref: string
+}
+
+interface ICardTemplace {
+  hash: string
+  from: string
+  to: string
+  tokenID: string
+}
+
+const POST_TEMPLATE = {
+  title: '',
+  content: [
+    [
+      {
+        tag: 'text',
+        text: '',
+      },
+      {
+        tag: 'a',
+        text: '',
+        href: '',
+      },
+    ],
+  ],
+}
+
+const CARD_TEMPLATE = {
+  config: {
+    wide_screen_mode: true,
+  },
+  elements: [
+    {
+      tag: 'div',
+      text: {
+        content:
+          '**Hash:**\n{Hash}\n**From:**\n{From}\n**To:**\n{To}\n**TokenID:**\n{TokenID}\n',
+        tag: 'lark_md',
+      },
+    },
+    {
+      tag: 'hr',
+    },
+    {
+      actions: [
+        {
+          tag: 'button',
+          text: {
+            content: '浏览器查看',
+            tag: 'plain_text',
+          },
+          type: 'primary',
+          url: '',
+        },
+      ],
+      tag: 'action',
+    },
+  ],
+  header: {
+    template: 'blue',
+    title: {
+      content: '⏰ 交易监控提醒',
+      tag: 'plain_text',
+    },
+  },
+}
+
 class Bot {
   private request: Request
 
@@ -7,11 +78,64 @@ class Bot {
     this.request = new Request(hookUrl)
   }
 
+  private _genPostTemplate(args: IPostTemplate) {
+    const _post_template = { ...POST_TEMPLATE }
+    _post_template.title = args.title
+    _post_template.content[0][0].text = args.text
+    _post_template.content[0][1].text = args.aText
+    _post_template.content[0][1].href = args.aHref
+    return _post_template
+  }
+
+  private _genCardTemplate(args: ICardTemplace) {
+    const _card_template = { ...CARD_TEMPLATE }
+
+    let url = `https://etherscan.io/tx/${args.hash}`
+    let content = _card_template.elements[0].text
+      ? _card_template.elements[0].text.content
+      : ''
+    content = content?.replace('{Hash}', args.hash)
+    content = content?.replace('{From}', args.from)
+    content = content?.replace('{To}', args.to)
+    content = content?.replace('{TokenID}', args.tokenID)
+
+    _card_template.elements[0].text
+      ? (_card_template.elements[0].text.content = content)
+      : null
+    _card_template.elements[2].actions
+      ? (_card_template.elements[2].actions[0].url = url)
+      : null
+
+    return _card_template
+  }
+
   async sendText(text: string) {
-    return await this.request.post('', {
+    const resp = await this.request.post('', {
       msg_type: 'text',
       content: { text },
     })
+
+    return typeof resp === 'string' ? false : true
+  }
+
+  async sendPost(args: IPostTemplate) {
+    const postTemplate = this._genPostTemplate(args)
+    const resp = await this.request.post('', {
+      msg_type: 'post',
+      content: { post: { zh_cn: postTemplate } },
+    })
+
+    return typeof resp === 'string' ? false : true
+  }
+
+  async sendCard(args: ICardTemplace) {
+    const cardTemplate = this._genCardTemplate(args)
+    const resp = await this.request.post('', {
+      msg_type: 'interactive',
+      card: cardTemplate,
+    })
+
+    return typeof resp === 'string' ? false : true
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import Bot from './bot'
-;(async function () {
-  const bot = new Bot(
-    'https://open.feishu.cn/open-apis/bot/v2/hook/935f3e40-9063-42c7-b4b6-0378a8ac3341'
-  )
 
-  for (let i = 0; i < 10; i++) {
-    const resp = await bot.sendText(i + '')
-    if (typeof resp === 'string') {
-      console.log('Error: ', resp)
-    } else {
-      console.log('Data: ', resp)
-    }
-  }
-})()
+const bot = new Bot(
+  'https://open.feishu.cn/open-apis/bot/v2/hook/a80fb039-3787-4e74-a6ac-3dca825b0fb2'
+)
+
+async function main() {
+  await bot.sendPost({
+    title: '交易监控提醒',
+    text: 'G-Drake 交易提醒',
+    aText: '浏览器查看',
+    aHref: 'https://baidu.com',
+  })
+
+  await bot.sendCard({
+    hash: '0xb9af9bcb08655077a204243fd29491714643030155212ad3f48527ee38cbfa67',
+    from: '0xdd8efab95bb34411b01ff9d1b95eb9ac1a4c1065',
+    to: '0x55623909fe2adf1f4ae4453f079dc19936505430',
+    tokenID: '15',
+  })
+}
+
+main()
