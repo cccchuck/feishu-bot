708cb6ae5a2d859b16a3c0335ee40fd377290eac
ChuckGao <chuckgao0223@gmail.com>
2022-10-08 13:55:18 +0800

    🔧 restruct

--- dist/utils/request.d.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
-declare class Request {
-    private instance;
-    constructor(hookURL: string);
-    initAxiosInstance(): void;
-    get(url: string, config?: AxiosRequestConfig<any> | undefined): Promise<AxiosResponse<any, any> | string>;
-    post(url: string, data?: any, config?: AxiosRequestConfig<any> | undefined): Promise<AxiosResponse<any, any> | string>;
-}
-export default Request;
